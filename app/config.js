09d611a8953e5a2f00202971375b1913c1734fab
TGA-Student <tga.student@hackreactor.com>
2015-07-30 12:21:46 -0700

    Revert "attempted to shoehorn mongo into our app"
    This reverts commit 60b88a1eba9319a7ae8839de5e527b9f78e0c474.

--- app/config.js
+++ app/config.js
@@ -1,72 +1,45 @@
-// var Bookshelf = require('bookshelf');
-var mongoose = require('mongoose');
+var Bookshelf = require('bookshelf');
 var path = require('path');
-var Schema = mongoose.Schema;
 
-// var db = Bookshelf.initialize({
-//   client: 'sqlite3',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'your_database_user',
-//     password: 'password',
-//     database: 'shortlydb',
-//     charset: 'utf8',
-//     filename: path.join(__dirname, '../db/shortly.sqlite')
-//   }
-// });
+var db = Bookshelf.initialize({
+  client: 'sqlite3',
+  connection: {
+    host: '127.0.0.1',
+    user: 'your_database_user',
+    password: 'password',
+    database: 'shortlydb',
+    charset: 'utf8',
+    filename: path.join(__dirname, '../db/shortly.sqlite')
+  }
+});
 
-var options = {
-  db: 'shortlymongoDb',
-  user: 'your_database_user',
-  pass: 'password',
+db.knex.schema.hasTable('urls').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('urls', function (link) {
+      link.increments('id').primary();
+      link.string('url', 255);
+      link.string('base_url', 255);
+      link.string('code', 100);
+      link.string('title', 255);
+      link.integer('visits');
+      link.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
 
-}
-
-mongoose.connect('mongodb:localhost/db', options);
-
-var LinksSchema = new Schema({
-  increments: {'$inc':{id:1}},
-  url: String,
-  base_url: String,
-  code: String,
-  title: String,
-  visits: Number
-  // timestamps: Date,
-})
-
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('base_url', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-var UsersSchema = new Schema({
-  increments: {'$inc':{id:1}},
-  username: {type: String, index: {unique: true}} //unique
-  password: String
-})
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+db.knex.schema.hasTable('users').then(function(exists) {
+  if (!exists) {
+    db.knex.schema.createTable('users', function (user) {
+      user.increments('id').primary();
+      user.string('username', 100).unique();
+      user.string('password', 100);
+      user.timestamps();
+    }).then(function (table) {
+      console.log('Created Table', table);
+    });
+  }
+});
 
 module.exports = db;
