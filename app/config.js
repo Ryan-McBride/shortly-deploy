63e52b237e8ab3e05c4a0b2ef347d3aba91d86bc
TGA-Student <tga.student@hackreactor.com>
2015-07-30 17:03:31 -0700

    successfully shoehorned mongodb into app

--- app/config.js
+++ app/config.js
@@ -1,72 +1,12 @@
-// var Bookshelf = require('bookshelf');
 var mongoose = require('mongoose');
-var path = require('path');
-var Schema = mongoose.Schema;
 
-// var db = Bookshelf.initialize({
-//   client: 'sqlite3',
-//   connection: {
-//     host: '127.0.0.1',
-//     user: 'your_database_user',
-//     password: 'password',
-//     database: 'shortlydb',
-//     charset: 'utf8',
-//     filename: path.join(__dirname, '../db/shortly.sqlite')
-//   }
-// });
+mongoURI = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/shortlydb';
+mongoose.connect(mongoURI);
 
-var options = {
-  db: 'shortlymongoDb',
-  user: 'your_database_user',
-  pass: 'password',
-
-}
-
-mongoose.connect('mongodb:localhost/db', options);
-
-var LinksSchema = new Schema({
-  increments: {'$inc':{id:1}},
-  url: String,
-  base_url: String,
-  code: String,
-  title: String,
-  visits: Number
-  // timestamps: Date,
-})
-
-// db.knex.schema.hasTable('urls').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('urls', function (link) {
-//       link.increments('id').primary();
-//       link.string('url', 255);
-//       link.string('base_url', 255);
-//       link.string('code', 100);
-//       link.string('title', 255);
-//       link.integer('visits');
-//       link.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
-
-var UsersSchema = new Schema({
-  increments: {'$inc':{id:1}},
-  username: {type: String, index: {unique: true}} //unique
-  password: String
-})
-
-// db.knex.schema.hasTable('users').then(function(exists) {
-//   if (!exists) {
-//     db.knex.schema.createTable('users', function (user) {
-//       user.increments('id').primary();
-//       user.string('username', 100).unique();
-//       user.string('password', 100);
-//       user.timestamps();
-//     }).then(function (table) {
-//       console.log('Created Table', table);
-//     });
-//   }
-// });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+  console.log('Mongodb connection open');
+});
 
 module.exports = db;
